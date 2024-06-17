ab442ff10084a4f954ce2733ab099de57d164040
Abdi J <abdij3196@gmail.com>
2024-06-17 20:05:54 +0700

    Add Next public base url

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,7 @@ const nextConfig = {
   reactStrictMode: true,
   env: {
     DATABASE_URL: process.env.DATABASE_URL,
+    NEXT_PUBLIC_BASE_URL: process.env.NEXT_PUBLIC_BASE_URL,
   },
   images: {
     remotePatterns: [
