094bec145ba5788ffa1a40de13195ebabfd31180
Abdi J <abdij3196@gmail.com>
2024-06-18 04:02:38 +0700

    mantap gw capek

--- src/app/api/destination/route.ts
+++ src/app/api/destination/route.ts
@@ -1,6 +1,8 @@
 import { prisma } from "@/lib/prisma";
+import { revalidatePath } from "next/cache";
 
 export async function GET() {
   const destinations = await prisma.destination.findMany();
+  revalidatePath("/");
   return new Response(JSON.stringify(destinations));
 }
